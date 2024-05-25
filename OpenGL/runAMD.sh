35fb332df2d6775a1998ca88856a04e24e2b1548
Felix Söderman <felix.soderman@netinsight.net>
2024-05-25 12:02:35 +0200

    Renamed dockerfile in subdir

--- OpenGL/runAMD.sh
+++ OpenGL/runAMD.sh
@@ -1,13 +1,4 @@
-#docker build -t game_of_life_glut -f Dockerfile .
-#xhost + local:docker
-#docker run --rm --device=/dev/kfd --device=/dev/dri --group-add video \
-#  --net=host \
-#  --env DISPLAY=$DISPLAY \
-#  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-#  --volume="$HOME/.Xauthority:/root/.Xauthority:rw"\
-#  game_of_life_glut 1920 1080
-
-docker build -t game_of_life_shader -f DockerfileComputeShader .
+docker build -t game_of_life_shader -f Dockerfile .
 xhost + local:docker
 docker run --device=/dev/kfd --device=/dev/dri --group-add video \
   --net=host \
